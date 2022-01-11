d3aca1f719c787fafd0acc772212b317e6490b6a
woodko <wlobanow@mail.ru>
2022-01-11 20:03:38 +0300

    Анимация спрайтов

--- pygame_lesson.py
+++ pygame_lesson.py
@@ -64,8 +64,10 @@ class Mob(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 #         self.image = pygame.Surface((30, 40))
 #         self.image.fill(red)
-        self.image = meteor_img
-        self.image.set_colorkey(black)
+#         self.image_orig = meteor_img
+        self.image_orig = random.choice(meteor_images)
+        self.image_orig.set_colorkey(black)
+        self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width * .85 / 2)
 #         pygame.draw.circle(self.image, red, self.rect.center, self.radius)
@@ -73,13 +75,31 @@ class Mob(pygame.sprite.Sprite):
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 8)
         self.speedx = random.randrange(-3, 3)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+    
     def update(self):
+        self.rotate()
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.top > height + 10 or self.rect.left < -25 or self.rect.right > width + 20:
             self.rect.x = random.randrange(width - self.rect.width)
             self.rect.y = random.randrange(-100, 40)
             self.speedy = random.randrange(1, 8)
+            # Вращение спрайтов
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            self.image = pygame.transform.rotate(self.image_orig, self.rot)
+            # Центр прямоугольника для ровного вращения
+            old_center = self.rect.center
+          #  self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
+            
             
 # Пули
 class Bullet(pygame.sprite.Sprite):
@@ -104,7 +124,13 @@ class Bullet(pygame.sprite.Sprite):
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
 player_img = pygame.image.load(path.join(img_dir, 'playerShip1_orange.png')).convert()
-meteor_img = pygame.image.load(path.join(img_dir, 'meteorBrown_med1.png')).convert()
+#meteor_img = pygame.image.load(path.join(img_dir, 'meteorBrown_med1.png')).convert()
+meteor_images = []
+meteor_list = ['meteorBrown_big1.png', 'meteorBrown_med1.png', 'meteorBrown_med3.png', 'meteorBrown_small1.png',
+               'meteorBrown_small2.png', 'meteorBrown_tiny1.png']
+for img in meteor_list:
+    meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
+    
 bullet_img = pygame.image.load(path.join(img_dir, 'laserRed16.png')).convert()
                      
         
