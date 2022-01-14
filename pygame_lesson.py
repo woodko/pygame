af318d117c46e793db5c414d1b69f783f822aa72
Влад <vlad@MacBook-Air.local>
2022-01-14 15:50:51 +0300

    Добавлен счет

--- pygame_lesson.py
+++ pygame_lesson.py
@@ -2,7 +2,7 @@ import pygame
 import random
 from os import path
 
-img_dir = path.join(path.dirname(__file__), 'img')
+img_dir = path.join(path.dirname(__file__), "img")
 
 
 width = 480
@@ -28,17 +28,17 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-#         self.image = pygame.Surface((50, 40))
-#         self.image.fill(green)
+        #         self.image = pygame.Surface((50, 40))
+        #         self.image.fill(green)
         self.image = pygame.transform.scale(player_img, (50, 38))
         self.image.set_colorkey(black)
         self.rect = self.image.get_rect()
         self.radius = 20
-#         pygame.draw.circle(self.image, red, self.rect.center, self.radius)
+        #         pygame.draw.circle(self.image, red, self.rect.center, self.radius)
         self.rect.centerx = width / 2
         self.rect.centery = height - 30
         self.speedx = 0
-        
+
     def update(self):
         self.speedx = 0
         keystate = pygame.key.get_pressed()
@@ -47,30 +47,31 @@ class Player(pygame.sprite.Sprite):
         if keystate[pygame.K_RIGHT]:
             self.speedx = 8
         self.rect.x += self.speedx
-        
+
         if self.rect.right > width:
             self.rect.right = width
         if self.rect.left < 0:
             self.rect.left = 0
-            
+
     def shoot(self):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
 
+
 # Моб
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-#         self.image = pygame.Surface((30, 40))
-#         self.image.fill(red)
-#         self.image_orig = meteor_img
+        #         self.image = pygame.Surface((30, 40))
+        #         self.image.fill(red)
+        #         self.image_orig = meteor_img
         self.image_orig = random.choice(meteor_images)
         self.image_orig.set_colorkey(black)
         self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
-        self.radius = int(self.rect.width * .85 / 2)
-#         pygame.draw.circle(self.image, red, self.rect.center, self.radius)
+        self.radius = int(self.rect.width * 0.85 / 2)
+        #         pygame.draw.circle(self.image, red, self.rect.center, self.radius)
         self.rect.x = random.randrange(width - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 8)
@@ -78,16 +79,21 @@ class Mob(pygame.sprite.Sprite):
         self.rot = 0
         self.rot_speed = random.randrange(-8, 8)
         self.last_update = pygame.time.get_ticks()
-    
+
     def update(self):
         self.rotate()
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if self.rect.top > height + 10 or self.rect.left < -25 or self.rect.right > width + 20:
+        if (
+            self.rect.top > height + 10
+            or self.rect.left < -25
+            or self.rect.right > width + 20
+        ):
             self.rect.x = random.randrange(width - self.rect.width)
             self.rect.y = random.randrange(-100, 40)
             self.speedy = random.randrange(1, 8)
             # Вращение спрайтов
+
     def rotate(self):
         now = pygame.time.get_ticks()
         if now - self.last_update > 50:
@@ -96,44 +102,51 @@ class Mob(pygame.sprite.Sprite):
             self.image = pygame.transform.rotate(self.image_orig, self.rot)
             # Центр прямоугольника для ровного вращения
             old_center = self.rect.center
-          #  self.image = new_image
+            #  self.image = new_image
             self.rect = self.image.get_rect()
             self.rect.center = old_center
-            
-            
+
+
 # Пули
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-#         self.image = pygame.Surface((10, 20))
-#         self.image.fill(yellow)
+        #         self.image = pygame.Surface((10, 20))
+        #         self.image.fill(yellow)
         self.image = bullet_img
         self.image.set_colorkey(black)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
         self.speedy = -10
+
     def update(self):
         self.rect.y += self.speedy
         # убить, если он заходит за верхнюю часть экрана
         if self.rect.bottom < 0:
             self.kill()
-            
-            
+
+
 # Загрузка всей игровой графики
-background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
+background = pygame.image.load(path.join(img_dir, "starfield.png")).convert()
 background_rect = background.get_rect()
-player_img = pygame.image.load(path.join(img_dir, 'playerShip1_orange.png')).convert()
-#meteor_img = pygame.image.load(path.join(img_dir, 'meteorBrown_med1.png')).convert()
+player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert()
+# meteor_img = pygame.image.load(path.join(img_dir, 'meteorBrown_med1.png')).convert()
 meteor_images = []
-meteor_list = ['meteorBrown_big1.png', 'meteorBrown_med1.png', 'meteorBrown_med3.png', 'meteorBrown_small1.png',
-               'meteorBrown_small2.png', 'meteorBrown_tiny1.png']
+meteor_list = [
+    "meteorBrown_big1.png",
+    "meteorBrown_med1.png",
+    "meteorBrown_med3.png",
+    "meteorBrown_small1.png",
+    "meteorBrown_small2.png",
+    "meteorBrown_tiny1.png",
+]
 for img in meteor_list:
     meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
-    
-bullet_img = pygame.image.load(path.join(img_dir, 'laserRed16.png')).convert()
-                     
-        
+
+bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
+
+
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -144,6 +157,19 @@ for i in range(8):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
+
+score = 0
+
+font_name = pygame.font.match_font("arial")
+
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, white)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
 
 # Цикл игры
 running = True
@@ -156,29 +182,31 @@ while running:
             if event.key == pygame.K_SPACE:
                 player.shoot()
 
-            
     # Обновление
     all_sprites.update()
-    
+
     # Проверка - попадание в моба
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
     for hit in hits:
+        score += 50 - hit.radius
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
-    
+
     # Проверка - не ударил ли моб игрока
-    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+    hits = pygame.sprite.spritecollide(
+        player, mobs, False, pygame.sprite.collide_circle
+    )
     if hits:
         running = False
-        
-    
-            
-    
+
     # Рендеринг
     screen.fill(blue)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, width / 2, 10)
+
+    # После отрисовки всего переворачиваем экран
     pygame.display.flip()
 
 
